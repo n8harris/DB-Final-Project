ccb1d13cdf71d8c95572ad5201bfe0c9103ee1a9
just4brown <justinbrown@cedarville.edu>
2014-04-24 21:53:59 -0400

    stuff

--- Eclipse Project/mlb/src/dataaccesslayer/HibernateUtil.java
+++ Eclipse Project/mlb/src/dataaccesslayer/HibernateUtil.java
@@ -10,6 +10,7 @@ import org.hibernate.cfg.Configuration;
 
 import bo.Player;
 import bo.Team;
+import bo.TeamSeason;
 import bo.TeamSeasonPlayer;
 
 public class HibernateUtil {
@@ -125,6 +126,64 @@ public class HibernateUtil {
 			e.printStackTrace();
 		}
 		return t;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<TeamSeason> retrieveTeamSeasonsById(Team t) {
+		List<TeamSeason> list = null;
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = session.getTransaction();
+		try {
+			tx.begin();
+			org.hibernate.Query query;
+			query = session.createQuery("from bo.TeamSeason where id.team = :team ");
+		    query.setParameter("team", t);
+		    list = query.list();
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static List<Player> retrieveRoster(Team t, String yid) {
+		List<Player> list = null;
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = session.getTransaction();
+		try {
+			tx.begin();
+			org.hibernate.Query query;
+			query = session.createQuery("select id.player from bo.TeamSeasonPlayer where id.team = :team and year = :yid");
+		    query.setParameter("team", t);
+		    query.setParameter("yid", yid);
+		    list = query.list();
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		//select from players where id is equal to player id from each TeamSeasonPlayer object in list
+//		List<Player> roster = new List<Player>();
+//		for(TeamSeasonPlayer tsp : list) {
+//			Player pid = tsp.getPlayer();
+//			try {
+//				tx.begin();
+//				org.hibernate.Query query;
+//				query = session.createQuery("from bo.Player where id.team = :team and year = :yid");
+//			    query.setParameter("team", t);
+//			    query.setParameter("yid", yid);
+//			    if (query.list().size()>0) {
+//			    	t = (Team) query.list().get(0);
+//			    }
+//				tx.commit();
+//			} catch (Exception e) {
+//				tx.rollback();
+//				e.printStackTrace();
+//			}
+//		}
+		return null;
 	}
 	
 	
